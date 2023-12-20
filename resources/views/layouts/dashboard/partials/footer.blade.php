f9ed30e2522b12cedf2803789e65a3ff48cda1d3
aldi <codealdi@gmail.com>
2023-12-20 17:39:34 +0700

    change footer for dashboard

--- resources/views/layouts/dashboard/partials/footer.blade.php
+++ resources/views/layouts/dashboard/partials/footer.blade.php
@@ -8,16 +8,5 @@
             BPD ABUJAPI SUMBAR, All rights reserved, Code by
             <a href="https://github.com/CodeAldi" target="_blank" class="footer-link fw-bolder">CodeAldi</a>
         </div>
-        <div>
-            <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
-            <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More
-                Themes</a>
-
-            <a href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/" target="_blank"
-                class="footer-link me-4">Documentation</a>
-
-            <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" target="_blank"
-                class="footer-link me-4">Support</a>
-        </div>
     </div>
 </footer>

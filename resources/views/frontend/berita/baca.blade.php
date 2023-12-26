92c9fd2300ed84af329810abed58520e6ca798e6
aldi <codealdi@gmail.com>
2023-12-26 18:35:02 +0700

    text break

--- resources/views/frontend/berita/baca.blade.php
+++ resources/views/frontend/berita/baca.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('content')
-<div class="container mt-5">
+<div class="container mt-5 text-break">
     <div class="atas mb-3 text-center">
         <h1 class="">{{ $berita[0]->judul_berita }}</h1>
         <p>Admin | {{ date_format($berita[0]->updated_at, 'd-M-Y H:i:s') }} WIB | {{ $berita[0]->kategori_berita->judul_kategori }}</p>
--- resources/views/frontend/kegiatan/baca.blade.php
+++ resources/views/frontend/kegiatan/baca.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('content')
 <div class="container mt-5">
-    <div class="atas mb-3 text-center">
+    <div class="atas mb-3 text-center text-break">
         <h1 class="">{{ $kegiatan[0]->judul_kegiatan }}</h1>
         <p>Admin | {{ date_format($kegiatan[0]->updated_at, 'd-M-Y H:i:s') }} WIB | {{
             $kegiatan[0]->kategori_kegiatan->judul_kategori }}</p>

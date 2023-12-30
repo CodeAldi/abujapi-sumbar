ab063d67b7b981afbde905458b595af1d3459c9c
aldi <codealdi@gmail.com>
2023-12-30 18:18:07 +0700

    index for gallery done

--- resources/views/admin/galeri/index.blade.php
+++ resources/views/admin/galeri/index.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.dashboard.main')
 @section('content')
 <div class="row row-cols-1 row-cols-md-3 g-4 mb-5">
+    @forelse ($galeri as $item)
     <div class="col">
         <div class="card">
             <div class="card-body">
-                <h2 class="card-title">title</h2>
-                <img class="card-img-top" src="#" />
+                <h4 class="card-title">{{ $item->judul_galeri }}</h4>
+                <img class="card-img-top" src="{{ asset('storage/'.$item->path) }}" />
             <p class="mt-2">Action : </p>
             <button type="button" class="btn btn-success rounded-pill btn-icon">
                 <span class="tf-icons bx bx-show"></span>
@@ -19,5 +20,8 @@
             </div>
         </div>
     </div>
+    @empty
+        
+    @endforelse
 </div>
 @endsection

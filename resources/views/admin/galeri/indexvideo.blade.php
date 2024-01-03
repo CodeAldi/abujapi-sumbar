80412ffb0c596dfe496543fcb4a020be666bdb15
aldi <codealdi@gmail.com>
2024-01-03 14:56:50 +0700

    galeri video show work

--- resources/views/admin/galeri/indexvideo.blade.php
+++ resources/views/admin/galeri/indexvideo.blade.php
@@ -10,15 +10,18 @@
             <div class="card-body">
                 <h4 class="card-title">{{ $item->judul_galeri_video }}</h4>
                 <p>Kategori : {{ $item->kategori_galeri->judul_kategori }}</p>
-                <iframe src="{{ 'https://www.youtube.com/embed/'.$item->link }}"  frameborder="0"></iframe>
+                <iframe src="{{ 'https://www.youtube.com/embed/'.$item->link }}" frameborder="0"></iframe>
                 <p class="mt-2">Action : </p>
-                <a href="{{ route('gallery.show',['galeri'=>$item]) }}" class="btn btn-success rounded-pill btn-icon">
+                <a href="#showmodal{{ $loop->iteration }}" data-bs-toggle='modal'
+                    class="btn btn-success rounded-pill btn-icon">
                     <span class="tf-icons bx bx-show"></span>
                 </a>
-                <a href="#updatemodal{{ $loop->iteration }}" data-bs-toggle='modal' class="btn btn-dark rounded-pill btn-icon">
+                <a href="#updatemodal{{ $loop->iteration }}" data-bs-toggle='modal'
+                    class="btn btn-dark rounded-pill btn-icon">
                     <span class="tf-icons bx bx-pencil"></span>
                 </a>
-                <form action="{{ route('gallery.video.destroy',['galerivideo'=>$item]) }}" method="post" class="d-inline">
+                <form action="{{ route('gallery.video.destroy',['galerivideo'=>$item]) }}" method="post"
+                    class="d-inline">
                     @csrf
                     @method('delete')
 
@@ -59,7 +62,8 @@
                         <select class="form-select" id="exampleFormControlSelect1" name="gallery_kategori">
                             <option selected disabled hidden>Please select....</option>
                             @forelse ($kategoriGaleri as $itemgaleri)
-                            <option value="{{ $itemgaleri->id }}" @if ($itemgaleri->id == $item->kategori_galeri_id) selected @endif>
+                            <option value="{{ $itemgaleri->id }}" @if ($itemgaleri->id == $item->kategori_galeri_id)
+                                selected @endif>
                                 {{ $itemgaleri->judul_kategori }}</option>
                             @empty
                             <option disabled>No Data</option>
@@ -69,7 +73,7 @@
                     <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">ID Youtube Video</label>
                         <input type="text" class="form-control" id="exampleFormControlInput1" name="link"
-                            value = "{{ $item->link }}" />
+                            value="{{ $item->link }}" />
                         <div class="form-text">contoh https://www.youtube.com/watch?v=qRODifJWIwE</div>
                         <div class="form-text">id video nya adalah qRODifJWIwE</div>
                     </div>
@@ -82,6 +86,47 @@
                 <button type="submit" class="btn btn-primary">Update Galeri Video</button>
             </div>
             </form>
+        </div>
+    </div>
+</div>
+@endforeach
+@foreach ($galeri as $item)
+<div class="modal fade" id="showmodal{{ $loop->iteration }}" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="tambahsingleTitle">Edit Galeri</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col">
+                        <label for="badan_usaha" class="form-label">Judul Galeri</label>
+                        <input type="text" id="badan_usaha" name="judul_galeri_video" class="form-control"
+                            value="{{ $item->judul_galeri_video }}" readonly disabled />
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlSelect1" class="form-label">Kategori</label>
+                    <select class="form-select" id="exampleFormControlSelect1" name="gallery_kategori" disabled>
+                        <option selected disabled hidden>Please select....</option>
+                        @forelse ($kategoriGaleri as $itemgaleri)
+                        <option value="{{ $itemgaleri->id }}" @if ($itemgaleri->id == $item->kategori_galeri_id)
+                            selected @endif>
+                            {{ $itemgaleri->judul_kategori }}</option>
+                        @empty
+                        <option disabled>No Data</option>
+                        @endforelse
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">ID Youtube Video</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" name="link"
+                        value="{{ $item->link }}" readonly disabled />
+                    <div class="form-text">contoh https://www.youtube.com/watch?v=qRODifJWIwE</div>
+                    <div class="form-text">id video nya adalah qRODifJWIwE</div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
